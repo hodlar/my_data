3cf9b3e08bddd66f00346062db6d8ac0d816c652
Carlos Rodri­guez <carlosrdz.isd@gmail.com>
2018-08-30 16:30:02 -0500

    changed labels

--- display_vectors.py
+++ display_vectors.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 size = int(input("Enter the number of points: "))
-with open('HeI_0.dat') as f:
+with open('hydrostatic.dat') as f:
     array = [[float(x) for x in line.split()] for line in f]
 
 z1 = []
 h1 = []
 
 for i in range(size):
-    z1.append(array[i][0])
-    h1.append(array[i][1])
+    z1.append(array[i][1])
+    h1.append(array[i][4])
 
 
-with open('HeI_2500000.dat') as f:
+with open('H_25000.dat') as f:
     array = [[float(x) for x in line.split()] for line in f]
 
 z2 = []
@@ -23,7 +23,8 @@ for i in range(size):
     z2.append(array[i][0])
     h2.append(array[i][1])
 
-with open('H_250000000000.dat') as f:
+
+with open('H_2500000.dat') as f:
     array = [[float(x) for x in line.split()] for line in f]
 
 z3 = []
@@ -33,5 +34,19 @@ for i in range(size):
     z3.append(array[i][0])
     h3.append(array[i][1])
 
-plt.plot(h1,z1, 'r--', h2, z2, 'g--', h3, z3, 'b--')
+with open('H_250000000000.dat') as f:
+    array = [[float(x) for x in line.split()] for line in f]
+
+z4 = []
+h4 = []
+
+for i in range(size):
+    z4.append(array[i][0])
+    h4.append(array[i][1])
+
+plt.figure(num='densidades')
+plt.xlabel('T')
+plt.ylabel('Density')
+plt.plot(z1,h1, 'm--', z2, h2, 'r--', z3, h3, 'g--', z4, h4, 'b--')
+#plt.plot(z1,h1, 'r--')
 plt.show()
